847e50a07ec7d8feed82fdf781b5153b163f4436
aviatormusic001-débogage <aviatormusic001@gmail.com>
2026-09-04 18:47:59 +0200

    Keep health route cacheable

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/up', static fn () => response('OK', 200))->name('health');
 
 /*
 |--------------------------------------------------------------------------
